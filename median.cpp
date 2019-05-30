52a44e9028df99166fbd72019f21dcdabbbeb867
davidbp13 <davidbp.13@gmail.com>
2019-05-30 14:15:13 +0200

    Implementation of median filter

--- median.cpp
+++ median.cpp
@@ -8,11 +8,36 @@
 // Christian De Schryver <schryver@eit.uni-kl.de>
 
 #include<image.h>
+#include<algorithm>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
 
 void medianFilter(const unsigned char input[][HEIGHT], unsigned char output[][HEIGHT], unsigned int width, unsigned int height)
 {
 
     //TODO: put your implementation here
+    int i = 0;
+    int window_width = 3;
+    int window_height = 3;
+    char window[window_width * window_height];
+
+    int edgex = window_width / 2;
+    int edgey = window_height / 2;
+
+    for (int x = edgex; x <= width - edgex; x++){
+        for (int y = edgex; y <= height - edgey; y++){
+            i = 0;
+            for (int fx = 0; fx < window_width; fx++){
+                for (int fy = 0; fy < window_height; fy++){
+                    window[i] = input[x + fx - edgex][y + fy - edgey];
+                    i++;
+                }
+            }
+            sort(window, window + (window_width * window_height));
+            output[x][y] = window[window_width * window_height / 2];
+        }
+    }
 
 }
 
